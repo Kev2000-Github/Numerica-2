12abc038b4b26259ddb81bd5ba8205973d3b871c
chengkev <chengkev2000@gmail.com>
2022-04-14 18:55:52 -0400

    added comments

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -9,12 +9,18 @@ function GaussSeidel(matrix, independentTerms, initialValues, tol)
   if isDiagonalDominant && isSquare
     solutions = initialValues;
     errors = zeros(1,N);
+    #sigue iterando hasta que el error relativo porcentual de todas las variables sean menor que el provisto
     do
       for j=1:N
         xant = solutions(j);
+        #obtiene la sumatoria ponderada de la fila exceptuando el de la variable a buscar
+        #el cual seria la variable dentro de la diagonal, y sigue la formula:
+        #x = (C - sumatoria)/a  (con C siendo el termino independiente y a el factor de x)
+        #Y el resultado se guarda inmediatamente en el vector para usarlo en las siguientes filas
         sumatoria = sum(matrix(j,:).*solutions) - matrix(j,j)*solutions(j);
         xi = (independentTerms(j) - sumatoria)/matrix(j,j);
         solutions(j) = xi;
+        #Se calcula el error relativo porcentual de la variable y se almacena en un arreglo
         err = abs((xi-xant)/xi)*100;
         errors(j) = err;
         xant = xi;
--- Jacobi.m
+++ Jacobi.m
@@ -10,16 +10,23 @@ function Jacobi(matrix, independentTerms, initialValues, tol)
     solutions = initialValues;
     newSolutions = solutions;
     errors = zeros(1,N);
+    #sigue iterando hasta que el error relativo porcentual de todas las variables sean menor que el provisto
     do
+      #calcula las incognitas fila por fila
       for j=1:N
         xant = solutions(j);
+        #obtiene la sumatoria ponderada de la fila exceptuando el de la variable a buscar
+        #el cual seria la variable dentro de la diagonal, y sigue la formula:
+        # x = (C - sumatoria)/a  (con C siendo el termino independiente y a el factor de x)
         sumatoria = sum(matrix(j,:).*solutions) - matrix(j,j)*solutions(j);
         xi = (independentTerms(j) - sumatoria)/matrix(j,j);
+        #Se calcula el error relativo porcentual de la variable y se almacena en un arreglo
         err = abs((xi-xant)/xi)*100;
         errors(j) = err;
         xant = xi;
         newSolutions(j) = xi;
       endfor
+      #Se actualiza las soluciones para el calculo de la siguiente iteracion
       solutions = newSolutions;
     until(all(errors < tol));
     disp("Solution:");
