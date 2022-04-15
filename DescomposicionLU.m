862b03628dc38ded5364b31a6a85fd5b9cc1035a
keyboard-girl <61607501+keyboard-girl@users.noreply.github.com>
2022-04-15 16:12:15 +0200

    Update DescomposicionLU.m
    Aun faltan detalles. En progreso...

--- DescomposicionLU.m
+++ DescomposicionLU.m
@@ -1,3 +1,21 @@
-function DescomposicionLU
+
+function descomposicion_lu(A);
   
-endfunction
+  [n,n]=size(A);
+  L=eye(n);
+  U=eye(n);
+  
+  for k=1:n
+    L(k,k)=1;
+    U(1,k)=A(1,k);
+    for i=k:n
+      U(k,i)=A(k,i)-L(k,1:k-1)*U(1:k-1,i);
+    endfor
+    
+    for i=k+1:n
+      L(i,k)=(A(i,k)-L(i,1:k-1)*U(1:k-1,k))/U(k,k)
+    endfor
+  endfor
+  L
+  U
+  
